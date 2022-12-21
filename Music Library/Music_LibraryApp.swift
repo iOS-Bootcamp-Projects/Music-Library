8e575b7200069dd0905fbb81aaa36f0cc5747ecf
aamer Essa <aamer.es12@gmail.com>
2022-12-21 13:00:24 +0300

    Initial Commit

---
+++ Music Library/Music_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_LibraryApp.swift
+//  Music Library
+//
+//  Created by Aamer Essa on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Music_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
